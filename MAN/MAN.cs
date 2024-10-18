b60a747790316d2dc58ea04752e4f3d225afb459
ABjokern <111576054+ABjokern@users.noreply.github.com>
2024-10-18 12:04:19 +0200

    Movement

--- MAN/MAN.cs
+++ MAN/MAN.cs
@@ -3,4 +3,21 @@ using System;
 
 public partial class MAN : CharacterBody2D
 {
+	private Vector2 newVelocity;
+	private int speed = 300;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		base._PhysicsProcess(delta);
+
+		GetDirection();
+		Velocity = newVelocity;
+		MoveAndSlide();
+	}
+
+	private void GetDirection()
+	{
+		newVelocity = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		newVelocity *= speed;
+	}
 }
