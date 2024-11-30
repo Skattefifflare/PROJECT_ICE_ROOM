92ce3401e20625616e7876fe255555ed1b5b8f5f
Casper <casper.lundstrom0623@gmail.com>
2024-11-30 15:02:25 +0100

    Cleanup and Testing

--- GenSpline.cs
+++ GenSpline.cs
@@ -5,54 +5,51 @@ using System.Linq;
 [Tool]
 public partial class GenSpline : Node2D
 {
-    int index = 0;
-    List<Vector2> shape = new List<Vector2>();
+    int index = 0; //index for which point to draw
+    List<Vector2> shape = new List<Vector2>(); //for testing
     public List<Vector2> ControlPoints = new List<Vector2>();
-    public int SegmentsPerCurve = 100;
-    private List<Vector2> _splinePoints = new List<Vector2>();
-    private Polygon2D ControllPolygon;
+    public int SegmentsPerCurve = 100; //Needs some testing for a better vaule?
+    private List<Vector2> _splinePoints = new List<Vector2>(); //the actual spline
+    private Polygon2D ControllPolygon; //for testing
     public override void _Ready()
+    {
+        Update();
+        AddChild(ControllPolygon);
+    }
+    private void Update()
     {
         if (shape.Count == 0)
         {
-            //shape.Add(new Vector2(200,200));
-            //shape.Add(new Vector2(0, 200));
-            //shape.Add(new Vector2(0, 0));
-            //shape.Add(new Vector2(200, 0));
             shape = new List<Vector2>
             {
                 new Vector2(0, 0),    
-                new Vector2(0, 50),   
-                new Vector2(100, 50), 
-                new Vector2(100, 30), 
-                new Vector2(100, 80), 
-                new Vector2(150, 80), 
-                new Vector2(150, 30),
-                new Vector2(100, 30)  
+                new Vector2(0, 200),   
+                new Vector2(300, 200), 
+                new Vector2(300, 300), 
+                new Vector2(500, 300), 
+                new Vector2(500, 100), 
+                new Vector2(400, 100),
+                new Vector2(400, 0)  
             };
-
             ControllPolygon = new Polygon2D
             {
                 Polygon = new Vector2[]
                 {
                     new Vector2(0, 0),
-                    new Vector2(0, 50),
-                    new Vector2(100, 50),
-                    new Vector2(100, 30),
-                    new Vector2(100, 80),
-                    new Vector2(150, 80),
-                    new Vector2(150, 30),
-                    new Vector2(100, 30)
+                    new Vector2(0, 200),
+                    new Vector2(300, 200),
+                    new Vector2(300, 300),
+                    new Vector2(500, 300),
+                    new Vector2(500, 100),
+                    new Vector2(400, 100),
+                    new Vector2(400, 0)
                 },
                 Color = new Color(0.5f, 0.5f, 0.5f, 0.5f)
             };
         }
-        AddChild(ControllPolygon);
-
         SetControlPoints();
         CalculateSpline();
     }
-    
     public override void _Draw()
     {
         if (_splinePoints.Count > 1)
@@ -62,10 +59,7 @@ public partial class GenSpline : Node2D
                 DrawLine(_splinePoints[i], _splinePoints[i + 1], Colors.White, 1);
             }
         }
-        
-        
     }
-    
     private void SetControlPoints()
     {
         var currentIndex = GetControlIndex();
@@ -98,10 +92,10 @@ public partial class GenSpline : Node2D
         }
         if (index < shape.Count) 
         {
-            _Ready();
+            Update();
         }
     }
-    //The catmull Rom algorithim to create the spline
+    //The catmull Rom algorithim to create the spline points
     private Vector2 CalculateCatmullRomPoint(float t, Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3)
     {
         float t2 = t * t;
@@ -122,10 +116,11 @@ public partial class GenSpline : Node2D
 
         return index switch
         {
-            _ when index == max => (index - 1, 0, 1),
-            _ when index == max - 1 => (index - 1, index + 1, 0),
-            _ when index == 0 => (max, index + 1, index + 2),
-            _ => (index - 1, index + 1, index + 2),
+            //Peak readability
+            _ when index == max => (index - 1, 0, 1), //drawing from last -> first point
+            _ when index == max - 1 => (index - 1, index + 1, 0), //--||-- second to last -> last point
+            _ when index == 0 => (max, index + 1, index + 2), //--||-- first -> second point
+            _ => (index - 1, index + 1, index + 2), //base case
         };
     }
 }
