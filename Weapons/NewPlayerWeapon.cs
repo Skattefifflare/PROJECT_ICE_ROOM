8771dc872481b80e987d22a7745bc4c31c1c5fba
Zunken <124305730+Skattefifflare@users.noreply.github.com>
2025-02-25 14:54:52 +0100

    _

--- Weapons/NewPlayerWeapon.cs
+++ Weapons/NewPlayerWeapon.cs
@@ -9,15 +9,18 @@ namespace Project_Ice_Room;
 public partial class NewPlayerWeapon : Weapon {
 
     Node2D parent;
+    AnimationPlayer animation_player;
 
     public override void _Ready() {
         base._Ready();
         parent = (Node2D)GetParent();
+        animation_player = (AnimationPlayer)FindChild("animation_player");
     }
 
     public override void _Process(double delta) {
         base._Process(delta);
         Rotate();
+        CheckInput();
     }
 
     private void Rotate() {
@@ -25,6 +28,11 @@ public partial class NewPlayerWeapon : Weapon {
             parent.Rotation = Mathf.Atan2(GetGlobalMousePosition().Y - parent.GlobalPosition.Y, GetGlobalMousePosition().X - parent.GlobalPosition.X);
         else
             parent.Rotation = Mathf.Atan2(GetGlobalMousePosition().Y - parent.GlobalPosition.Y, GetGlobalMousePosition().X - parent.GlobalPosition.X);
+    }
+    private void CheckInput() {
+        if (Input.IsActionJustPressed("left_click")) {
+            animation_player 
+        }
     }
 }
 
