f2a090b2a988f4853a3ad0d06bce0cefab461240
ABjokern <111576054+ABjokern@users.noreply.github.com>
2025-04-30 13:42:30 +0200

    UI

---
+++ Inventory/InventoryUI.cs
@@ -0,0 +1,44 @@
+using Godot;
+using System;
+
+public partial class InventoryUI : Control
+{
+    bool isOpen;
+    public override void _Ready()
+    {
+        Visible = false;
+        isOpen = false;
+
+        base._Ready();
+    }
+
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionJustPressed("toggle_inv"))
+        {
+            if (isOpen)
+            {
+                Close();
+                GD.Print("STAMG");
+            }
+            else
+            {
+                GD.Print("oppne");
+                Open();
+            }
+        }
+
+        base._Process(delta);
+    }
+
+    private void Open()
+    {
+        Visible = true;
+        isOpen = true;
+    }
+    private void Close()
+    {
+        Visible = false;
+        isOpen = false;
+    }
+}
